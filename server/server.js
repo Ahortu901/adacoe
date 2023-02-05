c08f8b56abe61d4de6b9958c523808a354aa6a68
Ahortu901 <aricktech.edu@gmail.com>
2023-02-05 10:36:05 +0000

    update

--- server/server.js
+++ server/server.js
@@ -31,15 +31,14 @@ app.post('/', async (req, res) => {
       temperature: 0.7, // Higher values means the model will take more risks.
       max_tokens: 256, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      best_of: 3,
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0.12, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      frequency_penalty: 0.19, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0.07 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
       bot: response.data.choices[0].text
     });
-
+ 
   } catch (error) {
     console.error(error)
     res.status(500).send(error || 'Something went wrong');
