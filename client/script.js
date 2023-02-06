2dd30f67850b40516ec9c75f45a7313320f44d48
Ahortu901 <aricktech.edu@gmail.com>
2023-02-06 08:33:03 +0000

    update server key

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://adacoe.onrender.com', {
+    const response = await fetch('https://adacoe1.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
